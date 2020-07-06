af879ab0c8da0320f97bcb161d6476eb3c7dcd9b
Fernando Treviño Compean <fernandocompean@Fernandos-MacBook-Pro.local>
2020-07-06 18:48:28 -0500

    [ADD]: routes added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Link }  from 'react-router-dom'
 
 const Header = () => {
     return ( 
         <nav className="navbar navbar-expand-lg navbar-dark bg-info justify-content-between">
         <div className="container">
-            <h1 > React - Redux - Axios
+            <h1 > 
+            <Link to={'/'} className="text-light">
+                React - Redux - Axios
+            </Link> 
             </h1>
         </div>
 
-        <a href="/productos/nuevo"
-            className="btn btn-warning nuevo-post d-block d-md-inline-block"
-        >Agregar Producto &#43;</a>
+            <Link to={"/productos/nuevo"}
+                className="btn btn-warning nuevo-post d-block d-md-inline-block"
+            >Agregar Producto &#43;</Link>
     </nav>
      );
 }
