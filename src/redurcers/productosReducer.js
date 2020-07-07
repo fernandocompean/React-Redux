deeccc14c3401795c9a7cb47654609893cbf84cc
Fernando Treviño Compean <fernandocompean@Fernandos-MacBook-Pro.local>
2020-07-07 18:27:58 -0500

    [add]: productos reducer added

---
+++ src/redurcers/productosReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    productos: [],
+    error: null,
+    loading: false
+}
+
+export default function(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+
+
+    }
+}
