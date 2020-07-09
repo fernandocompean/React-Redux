7b1ed5daf3db336c21cf7e4b5c6b0b22a530e191
Fernando Treviño Compean <fernandocompean@Fernandos-MacBook-Pro.local>
2020-07-08 20:30:52 -0500

    [feature] function imported and swal in product

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import { Link }  from 'react-router-dom'
+import { Link }  from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { borrarProductoAction } from '../actions/productoActions';
+import Swal from 'sweetalert2';
 
 const Producto = ({producto}) => {
     const {nombre, precio, id} = producto;
+
+    const dispatch = useDispatch();
+    
+    const confirmarEliminarProducto = id => {
+
+        Swal.fire({
+            title: '¿Estas seguro?',
+            text: "Un producto que se elimina no se puede recuperar.",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#6CC3D5',
+            cancelButtonColor: '#FF7851',
+            confirmButtonText: 'Si, eliminar!!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.value) {
+                dispatch( borrarProductoAction(id) );
+            }
+        });
+    }
     return ( 
         <tr>
             <td>{nombre}</td>
@@ -17,6 +40,7 @@ const Producto = ({producto}) => {
                 <button
                     type="button"
                     className="btn btn-danger"
+                    onClick={() => confirmarEliminarProducto(id)}
                 >
                     Eliminar
                 </button>
