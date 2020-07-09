66f882132650c1a48db6caac29818985c258bdb5
Fernando Treviño Compean <fernandocompean@Fernandos-MacBook-Pro.local>
2020-07-09 16:57:39 -0500

    [add] importing and calling alerts

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { crearNuevoProductoAction } from '../actions/productoActions';
+import { mostrarAlerta, ocultarAlertaAction } from '../actions/alertaActions';
 
     const NuevoProducto = ({history}) => {
 
@@ -12,6 +13,7 @@ import { crearNuevoProductoAction } from '../actions/productoActions';
 
         const cargando = useSelector( state => state.productos.loading );
         const error = useSelector( state => state.productos.error );
+        const alerta = useSelector(state => state.alerta.alerta);
         
 
         const agregarProducto = producto => dispatch( crearNuevoProductoAction(producto) );
@@ -20,8 +22,17 @@ import { crearNuevoProductoAction } from '../actions/productoActions';
             e.preventDefault();
 
             if(nombre.trim() === '' || precio <= 0) {
+
+                const alerta = {
+                    msg: 'Ambos campos son obligatorios',
+                    classes: 'alert alert-danger text-center text-uppercase p3'
+                }
+                dispatch( mostrarAlerta(alerta) );
+    
                 return;
             }
+
+            dispatch( ocultarAlertaAction() );
 
             agregarProducto({
                 nombre,
@@ -40,6 +51,8 @@ import { crearNuevoProductoAction } from '../actions/productoActions';
                         <h2 className="text-center mb-4 font-weight-bold">
                             Agregar Nuevo Producto
                         </h2>
+
+                        {alerta ? <p className={alerta.classes}> {alerta.msg} </p> : null }
 
                         <form
                              onSubmit={submitNuevoProducto}
